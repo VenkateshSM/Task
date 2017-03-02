2ff5d645698132d97caca6aa31b7d0e1564fb6a7
Venkatesh SM <suriyaa.sai@gmail.com>
2017-03-03 00:53:19 +0530

    Added Utils Directory for validation.

---
+++ utils/helpers.js
@@ -0,0 +1,8 @@
+let checkValidUrl = (url) => {
+    let regexp = /(ftp|http|https):\/\/(\w+:{0,1}\w*@)?(\S+)(:[0-9]+)?(\/|\/([\w#!:.?+=&%@!\-\/]))?/
+	return regexp.test(url);
+};
+
+module.exports = {
+  checkValidUrl : checkValidUrl
+};	
